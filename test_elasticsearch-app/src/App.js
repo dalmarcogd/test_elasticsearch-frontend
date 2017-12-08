4e4d020dfed58595aa33232b3dc1e251e953aa10
Guilherme Jacob Licinio Dalmarco <dalmarcogd@gmail.com>
2017-12-08 15:54:32 -0200

    test

--- test_elasticsearch-app/src/App.js
+++ test_elasticsearch-app/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Redirect } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import DashboardPage from './pages/dashboard/dashboard-page.js';
+import LoginPage from './pages/login/login-page.js'
+import NotFoundPage from './pages/notfound/notfound-page.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+	<div className="App">
+	  <BrowserRouter >
+        <Route path="/">            
+			<Redirect to="/dashboard"/>
+        </Route>
+        <Route path="/login" component={LoginPage}/>
+        <Route path="/dashboard" component={DashboardPage}/>
+		<Route path="/">
+            <Redirect to="/notfound"/>
+        </Route>
+        <Route path="notfound" component={NotFoundPage}/>
+      </BrowserRouter>
+	 </div>
+    
     );
   }
 }
---
+++ test_elasticsearch-app/src/pages/notfound/notfound-page.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './notfound-page.css';
+import _ from 'lodash'
+import { Sidebar, Segment, Menu, Image, Icon, Header, Grid, Rail, Sticky } from 'semantic-ui-react'
+
+const Placeholder = () => <Image src='/assets/images/wireframe/paragraph.png' />
+
+class NotfoundPage extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+  
+  handleContextRef = contextRef => this.setState({ contextRef })
+	
+  render() {
+	const { visible } = this.state
+	const { contextRef } = this.state
+    return ( 
+		<div>
+			<Menu stackable>
+				<Menu.Item onClick={this.toggleVisibility}>
+				  <Icon name='sidebar' />
+				</Menu.Item>
+
+				<Menu.Item
+				  name='features'
+				  onClick={this.toggleVisibility}>
+				  Features
+				</Menu.Item>
+
+				<Menu.Item
+				  name='testimonials'
+				  onClick={this.handleItemClick}>
+				  Testimonials
+				</Menu.Item>
+
+				<Menu.Item
+				  name='sign-in'
+				  onClick={this.handleItemClick}
+				>
+				  Sign-in
+				</Menu.Item>
+			</Menu>
+			<Sidebar.Pushable as={Segment}>
+				<Sidebar as={Menu}  animation='push' width='thin' visible={visible} icon='labeled' vertical>
+					<Menu.Item name='home'>
+						<Icon name='home' />
+						Home
+					</Menu.Item>
+				</Sidebar>
+				<Sidebar.Pusher>					
+					<Grid centered columns={3}>
+						<Grid.Column>
+						  <div ref={this.handleContextRef}>
+							<Segment>
+							  {_.times(10, i => <Placeholder key={i} />)}
+
+							  <Rail position='left'>
+								{_.times(3, i => <Placeholder key={i} />)}
+
+								<Sticky context={contextRef}>
+								  <Header as='h3'>Stuck Content</Header>
+								  <Image src='/assets/images/wireframe/image.png' />
+								</Sticky>
+							  </Rail>
+
+							  <Rail position='right'>
+								<Sticky context={contextRef}>
+								  <Header as='h3'>Stuck Content</Header>
+								  <Image src='/assets/images/wireframe/image.png' />
+								</Sticky>
+							  </Rail>
+							</Segment>
+						  </div>
+						</Grid.Column>
+					</Grid>
+				</Sidebar.Pusher>
+			</Sidebar.Pushable>
+		</div>
+    );
+  }
+}
+
+export default NotfoundPage;
